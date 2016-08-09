8ffb77c60a3866610ac568154988183d0a889e0e
Terrel Shumway <lernisto@users.no-reply.github.com>
2016-08-09 16:57:30 -0600

    download quiddich starter, edit to plain text

---
+++ quiddich/quidditch.rkt
@@ -0,0 +1,24 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname quidditch) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;; quidditch.rkt
+
+#;
+("PROBLEM:
+
+Imagine that you are designing a program that will keep track of
+your favorite Quidditch teams. (http://www.internationalquidditch.org/).
+
+Design a data definition to represent a list of Quidditch teams.
+")
+
+#;
+("
+PROBLEM:
+
+We want to know whether your list of favorite Quidditch teams includes
+UBC! Design a function that consumes ListOfString and produces true if 
+the list includes \"UBC\".
+")
+
