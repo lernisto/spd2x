f526c65add5561935c5793ca7db3deb7b10c616d
Terrel Shumway <moan-o-storm-beware-spoke@shumway.us>
2016-08-10 08:36:06 -0600

    constants and data definition for tuition graph

--- reference/tuition-graph.rkt
+++ reference/tuition-graph.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname tuition-graph-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname tuition-graph) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; tuition-graph.rkt
 
 #;
@@ -27,3 +27,55 @@ information in bar charts like this one:
 
 (require 2htdp/image)
 
+;; =========
+;; Constants
+
+(define BAR_WIDTH 30)
+(define CHART_HEIGHT 200)
+(define BAR_COLOR 'aliceblue)
+(define CHART_COLOR 'white)
+(define TEXT_SIZE 24)
+(define TEXT_COLOR 'darkblue)
+(define Y-SCALE 1/200) ;; TODO calculate this as CHART_HEIGHT/max-tuition
+
+;; ================
+;; Data Definitions
+
+(define-struct school (name tuition))
+;; School is (make-school name tuition)
+;; - compound: 2 fields
+;; - atomic distinct: String name - interp. name of school for chart
+;; - atomic distinct: Number tutition - interp. annual tuition in US dollars
+(define S1 (make-school "BYU Provo" (* 3 2500))) ;; oops. these are not international student fees, but are relevant to me
+(define S2 (make-school "BYU Idaho" (* 3 1915)))
+(define S3 (make-school "WGU" (* 2 3000)))
+
+#;
+(define (fn-for-school s)
+  (... (school-name s)
+       (school-tuition s)))
+;; Template rules used:
+;;  - compound: 2 fields
+
+;; ListOfSchool is one of:
+;; - empty
+;; - (cons School ListOfSchool)
+(define LS1 empty)
+(define LS2 (cons S1 (cons S2 (cons S3 empty))))
+
+#;
+(define (fn-for-los los)
+  (cond [(empty? los) (...)]
+        [else
+         (... (fn-for-school (first los))
+              (fn-for-los (rest los)))]))
+
+;; Template rules used:
+;;  - one of: 2 cases
+;;  - atomic distinct: empty
+;;  - compound: (cons School ListOfSchool)
+;;  - reference: (first los) is School
+;;  - self-reference: (rest los) is ListOfSchool
+
+;; =========
+;; Functions
