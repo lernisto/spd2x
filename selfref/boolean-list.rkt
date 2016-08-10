b65d73dbdaa30390611f32a2b0626456cc72c769
Terrel Shumway <moan-o-storm-beware-spoke@shumway.us>
2016-08-10 05:58:02 -0600

    finish boolean-list

--- selfref/boolean-list.rkt
+++ selfref/boolean-list.rkt
@@ -11,6 +11,22 @@
 
 Design a data definition to represent a list of booleans. Call it ListOfBoolean.
 ")
+;; ListOfBoolean is one of:
+;; - empty
+;; - (cons Boolean ListOfBoolean)
+(define LOB0 empty)
+(define LOB1 (cons #t (cons #f empty)))
+#;
+(define (fn-for-lob lob)
+  (cond [(empty? lob) (...)]
+        [else
+         (... (first lob)
+              (fn-for-lob (rest lob)))]))
+;; Template rules used:
+;; - one of: 2 cases
+;; - atomic distinct: empty
+;; - compound: (cons Boolean ListOfBoolean)
+;; - self-reference: (rest lob) is ListOfBoolean)
 
 ;; =================
 ;; Functions:
@@ -21,3 +37,26 @@ Design a function that consumes a list of boolean values and produces true
 if every value in the list is true. If the list is empty, your function 
 should also produce true. Call it all-true?
 ")
+;; ListOfBoolean -> Boolean
+;; produce true iff every element is true
+(check-expect (all-true? empty) #t)
+(check-expect (all-true? (cons #t (cons #t empty))) #t)
+(check-expect (all-true? (cons #t (cons #f empty))) #f)
+(check-expect (all-true? (cons #f (cons #t empty))) #f)
+
+;(define (all-true? lob) #f);stub
+
+;; Template from ListOfBoolean
+#;
+(define (all-true? lob)
+  (cond [(empty? lob) #t]
+        [else
+         (and (first lob)
+              (all-true? (rest lob)))]))
+
+;; This version seems to keep a smaller stack, but more steps in stepper
+;
+(define (all-true? lob)
+  (cond [(empty? lob) #t]
+        [(false? (first lob)) #f]
+        [else (all-true? (rest lob))]))
