011222fac76ad80e0f5b33ee2d64a5b73b0f1b32
Terrel Shumway <lernisto@users.no-reply.github.com>
2016-08-10 06:24:39 -0600

    completed largest (self-ref P5)
    It works, but I don't like it because of the double recursion.
    `let` would allow a better solution.

---
+++ selfref/largest.rkt
@@ -0,0 +1,54 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname largest-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;; largest-starter.rkt
+
+;; =================
+;; Data definitions:
+
+;; ListOfNumber is one of:
+;;  - empty
+;;  - (cons Number ListOfNumber)
+;; interp. a list of numbers
+(define LON1 empty)
+(define LON2 (cons 60 (cons 42 empty)))
+#;
+(define (fn-for-lon lon)
+  (cond [(empty? lon) (...)]
+        [else
+         (... (first lon)
+              (fn-for-lon (rest lon)))]))
+
+;; Template rules used:
+;;  - one of: 2 cases
+;;  - atomic distinct: empty
+;;  - compound: (cons Number ListOfNumber)
+;;  - self-reference: (rest lon) is ListOfNumber
+
+;; =================
+;; Functions:
+#;
+("PROBLEM:
+
+Design a function that consumes a list of numbers and produces the largest number 
+in the list. You may assume that all numbers in the list are greater than 0. If
+the list is empty, produce 0.
+")
+;; ListOfNumber -> Number
+;; produce the largest element
+(check-expect (largest empty) 0)
+(check-expect (largest (cons 42 (cons 60 empty))) 60)
+(check-expect (largest (cons 93 (cons 60 empty))) 93)
+
+;(define (largest lon) 0);stub
+;; Template from ListOfNumber
+(define (largest lon)
+  (cond [(empty? lon) 0]
+        [else
+         (if (> (first lon) (largest (rest lon)))
+             (first lon)
+             (largest (rest lon)))]))
+;; !!! this is a nasty implementation
+;; and yet, this is the same as the instructor's solution.
+;; I want let.
