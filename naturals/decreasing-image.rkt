04faf23c5cc4ea4e75ef2c4f9049316398ba50fc
Terrel Shumway <moan-o-storm-beware-spoke@shumway.us>
2016-08-10 19:18:12 -0600

    get decreasing-image starter

---
+++ naturals/decreasing-image.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname decreasing-image) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;; decreasing-image-starter.rkt
+#;
+("
+PROBLEM:
+ 
+ Design a function called decreasing-image that consumes a Natural n and produces an image of all the numbers 
+ from n to 0 side by side. 
+ 
+ So (decreasing-image 3) should produce
+")
+
+(check-expect (decreasing-image 3) (text "3210" 20 'black))
+
+
+
